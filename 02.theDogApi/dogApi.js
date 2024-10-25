9eafbfe46279d7a384ddebd4298287c997342886
jesualujan <lujan.oficial@gmail.com>
2024-10-24 21:02:58 -0600

    🎉 creación del proyecto

---
+++ 02.theDogApi/dogApi.js
@@ -0,0 +1,9 @@
+// JAVASCRIPT POR SI SOLO NO PUEDE HACERLO TODO
+// CONSUMIR UNA API
+// PARA ESTOY NECESITAMOS USAR A JAVASCRIPT PERO CON ESTEROIDES
+// PARA ELLO VAMOS A USAR NPM (NODE PACKAGE MANAGER) 
+// EN POCAS PALABRAS SON LOS PAQUETES QUE LE DARAN PODERES A JAVASCRIPT
+//* sitio: https://www.npmjs.com/ 
+//? Primer paso antes de hacer código debo inicializar npm, para ello
+//? vamos a usar el siguiente comando: npm init --y
+//? me va a crear un package.json (scripts, dependencias -> npm)
